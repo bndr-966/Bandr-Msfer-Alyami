5399e570e8d472482603bc9d0226a34fbcfd903b
Bandr Alrsheed <alyamibandr@gmail.com>
2026-05-21 10:35:46 +0300

    Delete python/week-4-H.W directory

--- python/week-4-H.W/problem3.py
+++
@@ -1,9 +0,0 @@
-a=input("Enter a value: ")
-b=input("Enter b value: ")
-c=input("Enter c value: ")
-if a == b == c:
-    print("Equilateral")
-elif a == b or a == c or b == c:
-    print("Isosceles")
-else:
-    print("Scalene")
